8efce922d126ed4f35d89d88561e77a791e6d165
Daniel Mateos <daniel@mateos.cc>
2019-02-20 00:00:12 +1100

    also chuck in an account connection to user, i use this kind of a thing a bit

--- engine/models.py
+++ engine/models.py
@@ -2,5 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Account(models.Model):
+    """
+    This is an account that will be linked to a django user
+    """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+
+
 class SomeModel(models.Model):
     pass
